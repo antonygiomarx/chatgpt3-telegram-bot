ccff0450ac324cd69f5cababb08b047978c5b420
Antony Giomar <antonygiomarx@gmail.com>
2023-02-05 14:04:06 -0600

    feat: added chatgpt and telegram modules :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ChatGptModule } from 'nestjs-chatgpt';
+import { TelegramModule } from './modules/webhook/telegram/telegram.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot(),
+    ChatGptModule.forRoot(process.env.CHATGPT_API_KEY),
+    TelegramModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
+  exports: [],
+  controllers: [AppController],
 })
 export class AppModule {}
